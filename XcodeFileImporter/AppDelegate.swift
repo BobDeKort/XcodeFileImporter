ac7847a25882d3589d09ac4bf74791246a335845
Bob De Kort <bogo@nodes.dk>
2019-02-27 16:08:39 +0100

    Initial Commit

---
+++ XcodeFileImporter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeFileImporter
+//
+//  Created by Bob De Kort on 27/02/2019.
+//  Copyright © 2019 Bob De Kort. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeFileImporter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeFileImporter
+//
+//  Created by Bob De Kort on 27/02/2019.
+//  Copyright © 2019 Bob De Kort. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
